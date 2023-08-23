c76e74f96d67b71913fb01950256db29a7eafd49
haqywijaya1 <137298829+haqywijaya1@users.noreply.github.com>
2023-08-24 00:02:25 +0800

    edit here and there
    edit here and there

--- src/app/components/editor.jsx
+++ src/app/components/editor.jsx
@@ -20,20 +20,14 @@ export const Editor = ({notesData = []}) => {
             </div>
             <div9>
             <figure>
-            <table>
-  <tr> 
-    <td><img
+            <img
      src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/ce17b737-054f-430a-98b8-f3c34455912a/dg5zvv7-221313b2-5d99-4d5b-aeb6-315c13c124e0.jpg/v1/fill/w_621,h_621,q_70,strp/fesbuk_pp_by_mhw1_dg5zvv7-350t-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjIxIiwicGF0aCI6IlwvZlwvY2UxN2I3MzctMDU0Zi00MzBhLTk4YjgtZjNjMzQ0NTU5MTJhXC9kZzV6dnY3LTIyMTMxM2IyLTVkOTktNGQ1Yi1hZWI2LTMxNWMxM2MxMjRlMC5qcGciLCJ3aWR0aCI6Ijw9NjIxIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.CkTUBi8sFUB3eJwo_l2t6nwcMtTDBHnRAxy3NdFBC38"
      alt="photoprofile"
      width="150px"                   
-        /></td>
-    <td>
-        <p22>“True warriors are fierce because their training is fierce.”
-— Miyamoto Musashi</p22>
-         </td>
-  </tr>
-</table>  
-     <figcaption>Modified by: Muhammad Haqy Wijaya (Haqy)</figcaption>
+        />
+        <p5>“True warriors are fierce because their training is fierce.”
+— Miyamoto Musashi</p5>
+         <figcaption>Modified by: Muhammad Haqy Wijaya (Haqy)</figcaption>
       </figure> 
       </div9>
         </div>      
